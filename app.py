64f41e4a8c3328c1dd254f7cff220fef91558270
Indraadhikari <indraadhikari0@gmail.com>
2025-09-19 12:09:02 +0200

    working on the excersice

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
+import datetime as dt
 import streamlit as st
 
-st.title("Hello, Streamlit!")
-st.write("This is your first Streamlit app.")
+st.title("Project work, part 1 - Dashboard basics")
+st.write("Streamlit app.")
 
-# Try changing the text below and saving the file!
-name = st.text_input("What's your name?")
-if name:
-    st.write(f"Hello, {name}!")
+df = pd.read_csv("open-meteo-subset.csv", encoding='UTF-8')
+#df.head()
+
+df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%dT%H:%M")
+df.head()
+
+
+fig, ax = plt.subplots(figsize=(8,4))
+ax.bar(df['time'].dt.month, df['precipitation (mm)'], color="skyblue", edgecolor="black")
+ax.set_xlabel("Month")
+ax.set_ylabel("Total Precipitation (mm)")
+ax.set_title("Precipitation per Month")
+
+st.pyplot
+
