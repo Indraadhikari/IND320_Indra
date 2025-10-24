669cdbfae69585d03904a07f897d6ec0b908a9e2
Indraadhikari <indraadhikari0@gmail.com>
2025-10-24 21:23:35 +0200

    Mongo DB access from streamlit app.

--- pages/3_dummy.py
+++ pages/3_dummy.py
@@ -1,23 +1,20 @@
 import streamlit as st
+from pymongo import MongoClient
 
-st.title("Data to Decision (Dummy headers)")
+# Retrieve secrets
+USR = st.secrets["mongo"]["username"]
+PWD = st.secrets["mongo"]["password"]
 
-st.header('About this course')
-text = '''The main emphasis will be on end-to-end data handling, analysis, and presentation for monitoring and decision support. Through various use cases, data will be accessed from databases, repositories, and/or sensors, filtered/cleaned, combined/analyzed, and displayed for the end user. Dashboards with plots and key performance indicators will be the main output.
+# Build connection string
 
-The course supports the UN's sustainability goals 4 (good education - industrial relevance for students), 9 (industry, innovation and infrastructure - better utilization of industrial data, internal and external infrastructure), 10 (less inequality - use of open platforms, programming languages and tools) and 12 (responsible consumption and production - streamlining and improving production processes).'''
+uri = "mongodb+srv://"+USR+":"+PWD+"@cluster0.wmoqhtp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-st.write(text)
+# Connect to MongoDB
+client = MongoClient(uri)
 
-st.header('Learning outcome')
-st.subheader('Knowledge')
-text2 = '''Theoretical aspects such as database queries, APIs, algorithms, etc. will be highlighted where there is a need, while practical problems are the main topic.'''
-st.write(text2)
+st.success("Successfully connected to MongoDB Atlas!")
 
-st.subheader('Skills')
-text3 = '''The student will learn to plan, implement and test a system for data capture, data processing and making it available to the end user. She will learn about current data types, data quality, as well as adopting tools needed to provide decision support and improved automation.'''
-st.write(text3)
+# Example: Read data from a collection
 
-st.subheader('General competence')
-text4 = '''By using mandatory tasks that build on each other and involve students at intersections through peer review, students will learn about short-term and long-term goals, the use of constructive criticism and continuous improvement of processes and products.'''
-st.write(text4)
+data = list(db["production_per_group"].find().limit(5))
+st.write(data)
